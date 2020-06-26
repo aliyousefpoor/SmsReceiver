e13da4789f6f5ec2c264058b84839d7534c9e7fe
Ali Yousefpoor <Ali@Mahdis-MBP.lan>
2020-06-26 13:12:22 +0430

    Move BroadCastReceiver to Manifest

--- app/src/main/java/com/example/smsreceiver/MainActivity.java
+++ app/src/main/java/com/example/smsreceiver/MainActivity.java
@@ -35,59 +35,23 @@ public class MainActivity extends AppCompatActivity {
 
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_SMS) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECEIVE_SMS}, MY_PERMISSIONS_REQUEST_RECEICE_SMS);
-        }else {
-            registerReceiver(broadcastReceiver, new IntentFilter("android.provider.Telephony.SMS_RECEIVED"));
+        } else {
         }
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
-        switch (requestCode){
+        switch (requestCode) {
 
-            case MY_PERMISSIONS_REQUEST_RECEICE_SMS:
-            {
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            case MY_PERMISSIONS_REQUEST_RECEICE_SMS: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
 
-                    registerReceiver(broadcastReceiver, new IntentFilter("android.provider.Telephony.SMS_RECEIVED"));
-                    Toast.makeText(this,"Thanks for Permmiting!",Toast.LENGTH_LONG).show();
-                }
-
-                else {
-                    Toast.makeText(this," Why didn't you permit me!",Toast.LENGTH_LONG).show();
+                    Toast.makeText(this, "Thanks for Permmiting!", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(this, " Why didn't you permit me!", Toast.LENGTH_LONG).show();
                 }
             }
         }
 
     }
-
-    private BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
-        @RequiresApi(api = Build.VERSION_CODES.M)
-        @Override
-        public void onReceive(Context context, Intent intent) {
-
-            if (intent.getAction().equals("android.provider.Telephony.SMS_RECEIVED")) {
-                Bundle bundle = intent.getExtras();           //---get the SMS message passed in---
-                SmsMessage[] msgs = null;
-                String msg_from;
-                if (bundle != null) {
-                    //---retrieve the SMS message received---
-                    try {
-                        Object[] pdus = (Object[]) bundle.get("pdus");
-                        msgs = new SmsMessage[pdus.length];
-                        for (int i = 0; i < msgs.length; i++) {
-                            msgs[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
-                            msg_from = msgs[i].getOriginatingAddress();
-                            String msgBody = msgs[i].getMessageBody();
-                            textView.setText(msgBody);
-                            Toast.makeText(context, msgBody, Toast.LENGTH_SHORT).show();
-                        }
-                    } catch (Exception e) {
-                        Log.d("Exception caught", e.getMessage());
-                    }
-                }
-            }
-        }
-    };
-
-
 }
